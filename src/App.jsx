e5acec880b62eedc11375975d81ccbc2a8a6bc09
Keshavendra Singh <skeshavendra@gmail.com>
2025-12-04 15:00:19 +0530

    Owner portal components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import { CarDetails } from './pages/CarDetails'
 import { Cars } from './pages/Cars'
 import { MyBooking } from './pages/MyBooking'
 import { Footer } from './compponents/Footer'
+import Layout from './pages/Owner/Layout'
+import Dashboard from './pages/Owner/Dashboard'
+import AddCar from './pages/Owner/AddCar'
+import ManageCars from './pages/Owner/ManageCars'
+import ManageBooking from './pages/Owner/ManageBooking'
 
 const App = () => {
   const [showLogin,setShowLogin]= useState(false)
@@ -19,6 +24,13 @@ const App = () => {
       <Route path="/cars/car-details/:id" element={<CarDetails />} />
       <Route path='/cars' element={<Cars/>}></Route>
       <Route path='/my-bookings' element={<MyBooking/>}></Route>
+
+      <Route path='/owner' element={<Layout></Layout>} >
+       <Route index element={<Dashboard/>}/>
+       <Route path='add-car' element={<AddCar/>}/>
+       <Route path='manage-car' element={<ManageCars/>}/>
+       <Route path='manage-bookings' element={<ManageBooking/>}/>
+      </Route>
 
       </Routes>
         
